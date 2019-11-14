7ee1e1f437ba639f5735c4ce40c446250244806b
Lucio Verane <verane64@gmail.com>
2019-11-13 18:57:15 -0600

    Updates

---
+++ precache-manifest.6f5f6ccbc074ea5437ff13275f518bbb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9330cb0a56f00725bf5ff9dcc8ce54dc",
+    "url": "/Clicky/index.html"
+  },
+  {
+    "revision": "873b382717c267b3a4f1",
+    "url": "/Clicky/static/css/main.be3135db.chunk.css"
+  },
+  {
+    "revision": "e753fc87211c54f9aa47",
+    "url": "/Clicky/static/js/2.089190e9.chunk.js"
+  },
+  {
+    "revision": "873b382717c267b3a4f1",
+    "url": "/Clicky/static/js/main.4750bd4c.chunk.js"
+  },
+  {
+    "revision": "71ca9dc95c8d5637c356",
+    "url": "/Clicky/static/js/runtime-main.65c52b2a.js"
+  }
+]);
